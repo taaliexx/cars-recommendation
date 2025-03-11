2bd7089b401496bfe13a8e481564f7e0a02085da
Natalia Afonina <nato4ka02@list.ru>
2025-03-11 12:04:23 +0300

    added loading dealers cars manually and via csv file

--- requests/routes.py
+++ requests/routes.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, UploadFile, File, HTTPException
 from sqlalchemy.orm import Session
 from database import get_db
 from models import Dealer, CarsForSale, SoldCars
@@ -7,12 +7,14 @@ from recommendations import get_knn_recommendations, save_recommendations_to_db
 from schemas import RecommendationBase
 import uuid
 import logging 
-from utils import verify_token_from_cookie
+import pandas as pd
+import io
+from utils import verify_token_from_cookie, preprocess_sold_data, EngineTypeEnum, ColorEnum, BodyTypeEnum, TransmissionEnum, PreviousOwnersEnum, detect_separator
 
 router = APIRouter(prefix="", tags=["requests"])
 
 @router.get("/cars_for_sale", response_model=List[dict])
-def get_cars(db: Session = Depends(get_db)):
+def get_cars_for_sale(db: Session = Depends(get_db)):
     """
     Cписок всех машин, которые можно приобрести для дальнейшей продажи.
     """
@@ -79,3 +81,62 @@ async def get_recommendations(db: Session = Depends(get_db), dealer_id: Dealer =
     save_recommendations_to_db(recommendations_dict, dealer_id, db)
 
     return recommendations_dict
+
+@router.post("/add_dealer_car")
+def add_dealer_car(manufacturer_name: str, model_name: str, transmission: TransmissionEnum,
+                   year_produced: int, odometer_value: float, price_usd: float,
+                   engine_type: EngineTypeEnum, body_type: BodyTypeEnum, color: ColorEnum, previous_owners: PreviousOwnersEnum, db: Session = Depends(get_db),
+                   dealer_id: str = Depends(verify_token_from_cookie)):
+    """Добавление проданной дилером машины вручную."""
+    dealer_uuid = uuid.UUID(dealer_id)
+    car = SoldCars(dealer_id=dealer_uuid, manufacturer_name=manufacturer_name, model_name=model_name,
+                    transmission=transmission, year_produced=year_produced, odometer_value=odometer_value, 
+                    price_usd=price_usd, engine_type=engine_type, body_type=body_type, color=color,
+                    previous_owners=previous_owners)
+    db.add(car)
+    db.commit()
+    return {"message": "Sold car added successfully!"}
+
+@router.post("/upload_sales")
+async def upload_sales(file: UploadFile = File(...), 
+                       dealer_id: str = Depends(verify_token_from_cookie), 
+                       db: Session = Depends(get_db)):
+    """Загрузка CSV-файла и сохранение проданных автомобилей в БД для текущего дилера."""
+    dealer_uuid = uuid.UUID(dealer_id)
+
+    # Читаем CSV в DataFrame
+    content = await file.read()
+    # df = pd.read_csv(io.StringIO(content.decode('utf-8')), delimiter=detect_separator(io.StringIO(content.decode('utf-8'))), header=0)
+    decoded_content = content.decode('utf-8')
+    separator = detect_separator(decoded_content)  # Определяем разделитель
+    df = pd.read_csv(io.StringIO(decoded_content), delimiter=separator, header=0)
+    if df.empty:
+        raise HTTPException(status_code=400, detail="Файл пуст или невалидный")
+
+    df = preprocess_sold_data(df)
+
+    # Загружаем в БД
+    try:
+        for _, row in df.iterrows():
+            sold_car = SoldCars(
+                dealer_id=dealer_uuid,
+                manufacturer_name=row["manufacturer_name"],
+                model_name=row["model_name"],
+                transmission=row["transmission"],
+                year_produced=row["year_produced"],
+                odometer_value=row["odometer_value"],
+                price_usd=row["price_usd"],
+                engine_type=row["engine_type"],
+                body_type=row["body_type"],
+                color=row["color"],
+                previous_owners=row["previous_owners"]
+            )
+            db.add(sold_car)
+
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        logging.error(f"Ошибка при загрузке данных: {str(e)}")
+        raise HTTPException(status_code=500, detail="Ошибка сервера при обработке файла")
+
+    return {"message": f"{len(df)} записей успешно загружены"}
--- utils.py
+++ utils.py
@@ -2,6 +2,12 @@ from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from fastapi import HTTPException, status, Request
 from globals import SECRET_KEY, ALGORITHM
+import pandas as pd
+import logging 
+from enum import Enum
+
+logging.basicConfig(level=logging.INFO, 
+                    format="%(asctime)s [%(levelname)s] %(message)s")
 
 def create_access_token(data: dict, expires_delta: timedelta):
     """Создаёт JWT-токен с истечением времени."""
@@ -28,3 +34,83 @@ def verify_token_from_cookie(request: Request):
         return dealer_id
     except JWTError:
         raise credentials_exception
+
+def preprocess_sold_data(df: pd.DataFrame):
+
+    target_columns = ['manufacturer_name', 'model_name', 'year_produced', 'transmission', 'color', 
+                  'odometer_value', 'price_usd', 'previous_owners', 'engine_type', 'body_type']
+    
+    body_types = ['universal', 'suv', 'sedan', 'hatchback', 'liftback', 'minivan', 'minibus', 'van', 'pickup', 'coupe', 'cabriolet', 'limousine']
+
+    colors = ['silver', 'blue', 'red', 'black', 'grey', 'other', 'brown', 'white', 'green', 'violet', 'orange', 'yellow']
+
+    df.dropna(inplace=True)
+
+    df['color'] = df['color'].apply(lambda x: 'grey' if x in 'gray' else x)
+    df['color'] = df['color'].apply(lambda x: x if x in colors else 'other')
+
+    df["previous_owners"] = df["previous_owners_num"].apply(lambda x: f'{x}' if x < 4 else 'more than 3')
+
+    df['body_type'] = df['body_type'].str.lower()
+    df['body_type'] = df['body_type'].apply(lambda x: 'coupe' if any(word in x.lower() for word in ['coupe', 'koup']) else x)
+    df['body_type'] = df['body_type'].apply(lambda x: 'suv' if any(word in x.lower() for word in ['suv']) else x)
+    df['body_type'] = df['body_type'].apply(lambda x: 'sedan' if any(word in x.lower() for word in ['sedan']) else x)
+    df['body_type'] = df['body_type'].apply(lambda x: 'pickup' if any(word in x.lower() for word in ['cab']) else x)
+    df['body_type'] = df['body_type'].apply(lambda x: 'van' if any(word in x.lower() for word in ['van']) else x)
+    df['body_type'] = df['body_type'].apply(lambda x: 'cabriolet' if any(word in x.lower() for word in ['convertible']) else x)
+    df['body_type'] = df['body_type'].apply(lambda x: 'universal' if any(word in x.lower() for word in ['wagon']) else x)
+    df['body_type'] = df['body_type'].apply(lambda x: 'other' if x not in body_types else x)
+
+    df = df[target_columns]
+
+    return df
+
+def detect_separator(content: str):
+    """
+    Определяет разделитель CSV-файла по его строковому содержимому.
+    """
+    sample = content[:1024]  # Берем первые 1024 символа
+    return ';' if sample.count(';') > sample.count(',') else ','
+
+class TransmissionEnum(str, Enum):
+    automatic = "automatic"
+    manual = "manual"
+
+class EngineTypeEnum(str, Enum):
+    gasoline = "gasoline"
+    diesel = "diesel"
+    
+class BodyTypeEnum(str, Enum):
+    universal = "universal"
+    suv = "suv"
+    sedan = "sedan"
+    hatchback = "hatchback"
+    liftback = "liftback"
+    minivan = "minivan"
+    minibus = "minibus"
+    van = "van"
+    pickup = "pickup"
+    coupe = "coupe"
+    cabriolet = "cabriolet"
+    limousine = "limousine"
+    other = "other"
+
+class ColorEnum(str, Enum):
+    silver = "silver"
+    blue = "blue"
+    red = "red"
+    black = "black"
+    grey = "grey"
+    brown = "brown"
+    white = "white"
+    green = "green"
+    violet = "violet"
+    orange = "orange"
+    yellow = "yellow"
+    other = "other"
+
+class PreviousOwnersEnum(str, Enum):
+    one = "1"
+    two = "2"
+    three = "3"
+    more_than_three = "more than 3"
